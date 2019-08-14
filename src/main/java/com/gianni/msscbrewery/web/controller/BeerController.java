2e27abac74d2546cbf4f14c1f48be6d59acec075
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2019-08-14 14:15:10 +0200

    fix RequestBody annotation missing in BeerController post and put handler

--- src/main/java/com/gianni/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/gianni/msscbrewery/web/controller/BeerController.java
@@ -25,7 +25,7 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity handlePost(BeerDto beerDto) {
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
         //TODO add hostname to url
@@ -34,7 +34,7 @@ public class BeerController {
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity handlePut(@PathVariable UUID beerId, BeerDto beerDto) {
+    public ResponseEntity handlePut(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
         beerService.updateBeer(beerId, beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
